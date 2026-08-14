5ff3f63f48f8cd18a1ca8f13bcf4a34c342d6ac1
Marlon Correia Bomfim Junior <42979884+marlaoza@users.noreply.github.com>
2026-08-14 10:29:41 -0300

    FEAT: scene transitions

--- Assets/Scripts/Game Controllers/SceneController.cs
+++ Assets/Scripts/Game Controllers/SceneController.cs
@@ -1,36 +1,124 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+
+
+public enum TransitionType
+{
+    None,
+    CrossFade
+}
+[Serializable]
+public struct TransitionProperties
+{
+    public GameObject gameObject;
+    public Animator animator;
+    public float time;
+}
+
+[Serializable]
+public struct TransitionEntry
+{
+    public TransitionType key;
+    public TransitionProperties value;
+}
 
 public class SceneController : MonoBehaviour
 {
     public static SceneController Instance { get; private set; }
 
+    TransitionType activeAnimation = TransitionType.None;
+
+    [SerializeField] private List<TransitionEntry> serializedTransitions = new List<TransitionEntry>();
+
+    Dictionary<TransitionType, TransitionProperties> transitions = new Dictionary<TransitionType, TransitionProperties>();
+
+    public void OnBeforeSerialize()
+    {
+        serializedTransitions.Clear();
+        foreach (var kvp in transitions)
+        {
+            serializedTransitions.Add(new TransitionEntry { key = kvp.Key, value = kvp.Value });
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        transitions.Clear();
+        for (int i = 0; i < serializedTransitions.Count; i++)
+        {
+            if (!transitions.ContainsKey(serializedTransitions[i].key))
+            {
+                transitions.Add(serializedTransitions[i].key, serializedTransitions[i].value);
+            }
+        }
+    }
+
     private void Awake()
     {
+       
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
+            transitions.Clear();
+            foreach (var kvp in serializedTransitions){ transitions.Add(kvp.key, kvp.value);}
+            serializedTransitions.Clear();
         }
         else
         {
             Destroy(gameObject);
         }
+
+        Debug.Log("Awake");
+        if (activeAnimation != TransitionType.None) { transitions[activeAnimation].gameObject.SetActive(false);}
+
     }
 
-    public void LoadScene(string sceneName)
+    public void LoadScene(int sceneIndex, TransitionType animationType = TransitionType.None)
     {
-        StartCoroutine(LoadSceneAsync(sceneName));
+        if (animationType != TransitionType.None) { 
+            transitions[animationType].gameObject.SetActive(true);
+            activeAnimation = animationType;
+        }
+
+        StartCoroutine(LoadSceneAsync(sceneIndex));
     }
 
-    private IEnumerator LoadSceneAsync(string sceneName)
+    private IEnumerator LoadSceneAsync(int sceneIndex)
     {
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
-
-        while (!asyncLoad.isDone)
+        float elapsedTime = 0f;
+        float targetTime = 0f;
+        if (activeAnimation != TransitionType.None)
         {
+            transitions[activeAnimation].animator.ResetTrigger("End");
+            transitions[activeAnimation].animator.SetTrigger("Start");
+            targetTime = transitions[activeAnimation].time;
+        }
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+        asyncLoad.allowSceneActivation = false;
+
+        while (elapsedTime < targetTime || asyncLoad.progress < 0.9f)
+        {
+            elapsedTime += Time.deltaTime;
             yield return null;
+        }
+        asyncLoad.allowSceneActivation = true;
+        if (activeAnimation != TransitionType.None)
+        {
+            transitions[activeAnimation].animator.ResetTrigger("Start");
+            transitions[activeAnimation].animator.SetTrigger("End");
+
+            yield return null;
+
+            while (transitions[activeAnimation].animator.IsInTransition(0) || transitions[activeAnimation].animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+            {
+                yield return null;
+            }
+            transitions[activeAnimation].animator.gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -1,34 +1,15 @@
-using System;
-using System.Collections;
-using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
     int activePanel = 0;
 
+    [SerializeField] SceneController sceneLoader;
+
     [SerializeField] GameObject[] panels;
 
     public void PlayGame(){
-        Debug.Log("startLoad");
-        StartCoroutine(LoadSceneAsyncCoroutine());
-    }
-
-    private IEnumerator LoadSceneAsyncCoroutine()
-    {
-        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-        UnityEngine.AsyncOperation operation = SceneManager.LoadSceneAsync(nextSceneIndex);
-
-        while (!operation.isDone)
-        {
-            float progress = Mathf.Clamp01(operation.progress / 0.9f);
-            Debug.Log($"{progress * 100}");
-
-            yield return null;
-        }
-
-        Debug.Log("endLoad");
+        sceneLoader.LoadScene(1, TransitionType.CrossFade);
     }
 
     public void QuitGame(){Application.Quit();}
